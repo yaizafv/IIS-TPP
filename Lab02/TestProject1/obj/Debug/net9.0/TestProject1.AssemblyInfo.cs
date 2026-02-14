49dd3f3b41db42d84fc0f8dca41036acf04ec678
yaiza <yaizafv2004@gmail.com>
2026-02-14 20:34:01 +0100

    Commit Lab03

--- Lab02/TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ Lab02/TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c3ec11af75bb7cb0982643a015ac634a0a7a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817d196e6321fc3ff087b974d1c4156883acb912")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
