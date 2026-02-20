88de40b3fc87dfc59e49ff283fea279df89b66ef
yaiza <yaizafv2004@gmail.com>
2026-02-20 11:01:37 +0100

    Commit Lab04

--- Lab03/SortedList/obj/Debug/net9.0/SortedList.AssemblyInfo.cs
+++ Lab03/SortedList/obj/Debug/net9.0/SortedList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SortedList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d99a79b9b1f79d11d5030f3e5b4cf81cc051240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85f5f64f5f46aa03f3ceff90b822b2c50759efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SortedList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SortedList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab04/IEnumerable/obj/Debug/net9.0/IEnumerable.AssemblyInfo.cs
+++ Lab04/IEnumerable/obj/Debug/net9.0/IEnumerable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IEnumerable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d99a79b9b1f79d11d5030f3e5b4cf81cc051240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85f5f64f5f46aa03f3ceff90b822b2c50759efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("IEnumerable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IEnumerable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
