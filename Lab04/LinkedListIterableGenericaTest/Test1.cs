bb8d26ff56736da9388a596f89dbe3791ad621d7
yaiza <yaizafv2004@gmail.com>
2026-02-24 12:27:08 +0100

    Commit Lab04

---
+++ Lab04/LinkedListIterableGenericaTest/Test1.cs
@@ -0,0 +1,33 @@
+﻿namespace LinkedListIterableGenericaTest;
+using LinkedListIterableGenerica;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestCasosNormales()
+    {
+        // Test con Enteros
+        LinkedList<int> listaInt = new LinkedList<int>();
+        listaInt.Add(1);
+        listaInt.Add(2);
+        listaInt.Add(3);
+
+        Console.WriteLine("--- Test Enteros ---");
+        foreach (int n in listaInt)
+        {
+            Console.WriteLine($"Valor: {n}"); // Debería imprimir 1, 2, 3
+        }
+
+        // Test con Strings (Tipos de referencia)
+        LinkedList<string> listaStr = new LinkedList<string>();
+        listaStr.Add("Hola");
+        listaStr.Add("Mundo");
+
+        Console.WriteLine("\n--- Test Strings ---");
+        foreach (string s in listaStr)
+        {
+            Console.WriteLine($"Texto: {s}");
+        }
+    }
+}
