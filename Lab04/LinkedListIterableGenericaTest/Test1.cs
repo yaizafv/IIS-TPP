287899a6f57eba7915fbb7f40a1aa93ab48290ac
yaiza <yaizafv2004@gmail.com>
2026-02-24 16:12:08 +0100

    Commit Lab04

--- Lab04/LinkedListIterableGenericaTest/Test1.cs
+++ Lab04/LinkedListIterableGenericaTest/Test1.cs
@@ -17,19 +17,36 @@ public sealed class Test1
         {
             Console.WriteLine($"Valor: {n}");
         }
+        Assert.AreEqual(1, listaInt[0]);
+        Assert.AreEqual(2, listaInt[1]);
+        Assert.AreEqual(3, listaInt[2]);
     }
 
     [TestMethod]
     public void testStrings()
     {
-        LinkedList<String> listaStr = new LinkedList<string>();
+        LinkedList<String> listaStr = new LinkedList<String>();
         listaStr.Add("Hola");
         listaStr.Add("Mundo");
 
-        Console.WriteLine("\n--- Test Strings ---");
         foreach (string s in listaStr)
         {
             Console.WriteLine($"Texto: {s}");
+        }
+        Assert.AreEqual("Hola", listaStr[0]);
+        Assert.AreEqual("Hola", listaStr[1]);
+    }
+
+    public void testDouble()
+    {
+        LinkedList<double> listaInt = new LinkedList<double>();
+        listaInt.Add(1.5);
+        listaInt.Add(2.3);
+        listaInt.Add(3.7);
+
+        foreach (int n in listaInt)
+        {
+            Console.WriteLine($"Valor: {n}");
         }
     }
 }
